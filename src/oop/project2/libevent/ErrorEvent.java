a6f106e17c229259922724ff33f71e4047fa12ae
Dugan Dobbs <ddobbs@Dugans-MacBook-Pro.local>
2017-11-25 11:54:48 -0600

    Fixing ErrorEvent

--- src/oop/project2/libevent/ErrorEvent.java
+++ src/oop/project2/libevent/ErrorEvent.java
@@ -5,40 +5,22 @@ package oop.project2.libevent;
  * @author ddobbs
  */
 public class ErrorEvent extends LibEvent{
-    private int errorType;
-    final private String errorText[] = {"Book not found.",
-                                        "No books in stock.",
-                                        "User not found.",
-                                        "User has max books checked out.",
-                                        "Invalid input stream.",
-                                        "",
-                                        "",
-                                        "",
-                                        "",
-                                        "",
-                                        ""};
-    final private int numStatus = 5;
-    
+    LibError error;
 
     public ErrorEvent(LibError err){
         super(1,err.getSummary());
-        this.setErrorType(errorType);
-    }
-    
-    private void setErrorType(int errorType){
-        this.errorType = (errorType <=0 || errorType >= numStatus) ? errorType : -3;
     }
     
     public int getErrorType(){
-        return this.errorType;
+        return error.getType();
     }
     
     public String getErrorText(){
-        return this.errorText[this.errorType];
+        return error.getStatusText();
     }
 
     @Override
     public String getSummary() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return error.getSummary();
     }
 }
