1de0b1ba7fa772970554b8a9345630a8fd949e78
Dugan Dobbs <ddobbs@Dugans-MacBook-Pro.local>
2017-11-26 15:15:20 -0600

    I hate concurrency

--- src/oop/project2/LendingLibraryImpl.java
+++ src/oop/project2/LendingLibraryImpl.java
@@ -3,45 +3,36 @@ package oop.project2;
 import java.awt.EventQueue;
 
 public class LendingLibraryImpl {
-    LendingLibraryGUI GUI;
-    DBThread          DBs;
-    InputBuilder      Inp;
-    EventBuilder      Evt;
 
 	/**
 	 * Launch the application.
 	 */ 
 	public static void main(String[] args) { 
-            LendingLibraryImpl session = new LendingLibraryImpl();
-            session.init();
-            session.start();
-	}
-        
-        public LendingLibraryImpl(){
-            DBs = new DBThread();
-            Inp = new InputBuilder();
-            Evt = new EventBuilder();
+            DBThread          DBs = new DBThread();
+            InputBuilder      Inp = new InputBuilder();
+            EventBuilder      Evt = new EventBuilder();
+            
+            Thread DB = new Thread(DBs);
+            Thread In = new Thread(Inp);
+            Thread Ev = new Thread(Evt);
+            Thread[] threads = {DB,In,Ev};
+            
             EventQueue.invokeLater(new Runnable() {
  			public void run() {
  				try {
- 					GUI = new LendingLibraryGUI(DBs,Inp,Evt);
+ 					LendingLibraryGUI GUI = new LendingLibraryGUI(DBs,Inp,Evt);
  					GUI.setVisible(true);
  				} catch (Exception e) {
  					e.printStackTrace();
  				}
  			}
  		});
-        }
-        
-        public void init(){
+            
             DBs.associate(DBs,Inp,Evt);
             Evt.associate(DBs,Inp,Evt);
-        }
-        
-        public void start(){
             
-            EventQueue.invokeLater(DBs);
-            EventQueue.invokeLater(Inp);
-            EventQueue.invokeLater(Evt);
-        }
+            for(Thread t: threads){
+                t.start();
+            }
+	}
 }
